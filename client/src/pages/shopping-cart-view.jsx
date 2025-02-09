70e6de43d46b96b03feaf43d297296d260e97582
wessel <wesselvandalen@gmail.com>
2025-02-10 00:08:57 +0100

    Endret footeren og lagt til produkt visning

--- client/src/pages/shopping-cart-view.jsx
+++ client/src/pages/shopping-cart-view.jsx
@@ -6,13 +6,23 @@ import { makeNumbersReadable } from '../service/utils.js';
 import Footer from '../components/footer.jsx';
 import { strokeWidth } from '../service/config.js';
 import LoadingScreen from '../components/loading-screen.jsx';
+import { fetchProducts } from '../service/product-service.js';
+import ProductBlock from '../components/product-block.jsx';
+import { getRandomItems } from '../service/product-service.js';
 
 export default function ShoppingCartView() {
     const [cartItems, setCartItems] = useState([]);
+    const [randomProducts, setRandomProducts] = useState([]);
 
     useEffect(() => {
         fetchShoppingCartItems();
+        getProductsByCategory();
     }, []);
+
+    async function getProductsByCategory() {
+        const data = await fetchProducts();
+        setRandomProducts(getRandomItems(data, 3));
+    }
 
     const fetchShoppingCartItems = () => {
         const items = getShoppingCart();
@@ -23,33 +33,13 @@ export default function ShoppingCartView() {
         window.location.replace(`/bestilling/`);
     };
 
-    if (!cartItems) {
+    if (!cartItems || !randomProducts) {
         return (
             <LoadingScreen
                 message="Vi henter handlekurven for deg..."
             />
         );
     }
-
-    if (cartItems.length === 0) {
-        return <div className="shopping-cart-view">
-            <div className="shopping-cart-container">
-                <div className="shopping-cart-content">
-                    <div className="bag-info-container">
-                        <div>
-                            <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5.52 2.64L3.96 4.72C3.65102 5.13198 3.49652 5.33797 3.50011 5.51039C3.50323 5.66044 3.57358 5.80115 3.69175 5.89368C3.82754 6 4.08503 6 4.6 6H19.4C19.915 6 20.1725 6 20.3083 5.89368C20.4264 5.80115 20.4968 5.66044 20.4999 5.51039C20.5035 5.33797 20.349 5.13198 20.04 4.72L18.48 2.64M5.52 2.64C5.696 2.40533 5.784 2.288 5.89552 2.20338C5.9943 2.12842 6.10616 2.0725 6.22539 2.03845C6.36 2 6.50667 2 6.8 2H17.2C17.4933 2 17.64 2 17.7746 2.03845C17.8938 2.0725 18.0057 2.12842 18.1045 2.20338C18.216 2.288 18.304 2.40533 18.48 2.64M5.52 2.64L3.64 5.14666C3.40254 5.46328 3.28381 5.62159 3.1995 5.79592C3.12469 5.95062 3.07012 6.11431 3.03715 6.28296C3 6.47301 3 6.6709 3 7.06666L3 18.8C3 19.9201 3 20.4802 3.21799 20.908C3.40973 21.2843 3.71569 21.5903 4.09202 21.782C4.51984 22 5.07989 22 6.2 22L17.8 22C18.9201 22 19.4802 22 19.908 21.782C20.2843 21.5903 20.5903 21.2843 20.782 20.908C21 20.4802 21 19.9201 21 18.8V7.06667C21 6.6709 21 6.47301 20.9628 6.28296C20.9299 6.11431 20.8753 5.95062 20.8005 5.79592C20.7162 5.62159 20.5975 5.46328 20.36 5.14667L18.48 2.64M16 10C16 11.0609 15.5786 12.0783 14.8284 12.8284C14.0783 13.5786 13.0609 14 12 14C10.9391 14 9.92172 13.5786 9.17157 12.8284C8.42143 12.0783 8 11.0609 8 10" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                            <h2>Handlekurv</h2>
-                        </div>
-                        <p>Din handlekurv fylt med smart teknologi – klart til å bli levert!</p>
-                        <p>Det er ganske stille her...</p>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
-        </div>
-    };
 
     return (
         <div className="shopping-cart-view">
@@ -63,29 +53,63 @@ export default function ShoppingCartView() {
                             <h2>Handlekurv</h2>
                         </div>
                         <p>Din handlekurv fylt med smart teknologi – klart til å bli levert!</p>
+                        {cartItems.length === 0 ? <p>Det er ganske stille her...</p> : null }
                     </div>
-                    <div className="shopping-cart-items">
-                        {cartItems.map((product) => {
-                            return <ShoppingCartItem
-                                key={product.productId}
-                                productId={product.productId}
-                                amount={product.productAmount}
-                            />
-                        })}
+
+                    {cartItems.length === 0 ?
+                        null
+                        :
+                        <>
+                            <div className="shopping-cart-items">
+                                {cartItems.map((product) => {
+                                    return <ShoppingCartItem
+                                        key={product.productId}
+                                        productId={product.productId}
+                                        amount={product.productAmount}
+                                    />
+                                })}
+                            </div>
+                            <div className="shopping-cart-summary">
+                                <button className="shopping-cart-view-order-button" onClick={handleOrderButton}>
+                                    <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M20.5 7.27783L12 12.0001M12 12.0001L3.49997 7.27783M12 12.0001L12 21.5001M21 16.0586V7.94153C21 7.59889 21 7.42757 20.9495 7.27477C20.9049 7.13959 20.8318 7.01551 20.7354 6.91082C20.6263 6.79248 20.4766 6.70928 20.177 6.54288L12.777 2.43177C12.4934 2.27421 12.3516 2.19543 12.2015 2.16454C12.0685 2.13721 11.9315 2.13721 11.7986 2.16454C11.6484 2.19543 11.5066 2.27421 11.223 2.43177L3.82297 6.54288C3.52345 6.70928 3.37369 6.79248 3.26463 6.91082C3.16816 7.01551 3.09515 7.13959 3.05048 7.27477C3 7.42757 3 7.59889 3 7.94153V16.0586C3 16.4013 3 16.5726 3.05048 16.7254C3.09515 16.8606 3.16816 16.9847 3.26463 17.0893C3.37369 17.2077 3.52345 17.2909 3.82297 17.4573L11.223 21.5684C11.5066 21.726 11.6484 21.8047 11.7986 21.8356C11.9315 21.863 12.0685 21.863 12.2015 21.8356C12.3516 21.8047 12.4934 21.726 12.777 21.5684L20.177 17.4573C20.4766 17.2909 20.6263 17.2077 20.7354 17.0893C20.8318 16.9847 20.9049 16.8606 20.9495 16.7254C21 16.5726 21 16.4013 21 16.0586Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                        <path d="M16.5 9.5L7.5 4.5" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                    Legg inn bestilling
+                                </button>
+                                <p className={"total-price"}>{makeNumbersReadable(getTotalPrice())} kr <span className={"tax-text"}>*Fraktkostnad vil bli beregnet senere.</span></p>
+                            </div>
+                        </>
+                    }
+
+                    <div className="product-detail-view-recommendations">
+                        <h3 className='product-detail-view-recommendations-title'>
+                            {cartItems.length === 0 ?
+                                <>Her er noe inspirasjon for handlekurven din</>
+                                :
+                                <>Anbefalte produkt for å gjøre handlekurven din perfekt</>
+                            }
+                        </h3>
+                        <div className="recommendations-grid">
+                            {randomProducts.map((product) => {
+                                return <ProductBlock
+                                    key={product.id}
+                                    id={product.id}
+                                    title={product.name}
+                                    imagePath={product.imagePaths[0]}
+                                    description={product.description}
+                                    price={product.price}
+                                    ratings={product.ratings}
+                                    averageRating={product.averageRating}
+                                    numberOfRatings={product.numberOfRatings}
+                                />
+                            })}
+                        </div>
                     </div>
-                    <div className="shopping-cart-summary">
-                        <button className="shopping-cart-view-order-button" onClick={handleOrderButton}>
-                            <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M20.5 7.27783L12 12.0001M12 12.0001L3.49997 7.27783M12 12.0001L12 21.5001M21 16.0586V7.94153C21 7.59889 21 7.42757 20.9495 7.27477C20.9049 7.13959 20.8318 7.01551 20.7354 6.91082C20.6263 6.79248 20.4766 6.70928 20.177 6.54288L12.777 2.43177C12.4934 2.27421 12.3516 2.19543 12.2015 2.16454C12.0685 2.13721 11.9315 2.13721 11.7986 2.16454C11.6484 2.19543 11.5066 2.27421 11.223 2.43177L3.82297 6.54288C3.52345 6.70928 3.37369 6.79248 3.26463 6.91082C3.16816 7.01551 3.09515 7.13959 3.05048 7.27477C3 7.42757 3 7.59889 3 7.94153V16.0586C3 16.4013 3 16.5726 3.05048 16.7254C3.09515 16.8606 3.16816 16.9847 3.26463 17.0893C3.37369 17.2077 3.52345 17.2909 3.82297 17.4573L11.223 21.5684C11.5066 21.726 11.6484 21.8047 11.7986 21.8356C11.9315 21.863 12.0685 21.863 12.2015 21.8356C12.3516 21.8047 12.4934 21.726 12.777 21.5684L20.177 17.4573C20.4766 17.2909 20.6263 17.2077 20.7354 17.0893C20.8318 16.9847 20.9049 16.8606 20.9495 16.7254C21 16.5726 21 16.4013 21 16.0586Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                <path d="M16.5 9.5L7.5 4.5" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                            Legg inn bestilling
-                        </button>
-                        <p className={"total-price"}>{makeNumbersReadable(getTotalPrice())} kr <span className={"tax-text"}>*Fraktkostnad vil bli beregnet senere.</span></p>
-                    </div>
+
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
